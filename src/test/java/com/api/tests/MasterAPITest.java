d49d0faa9245234706832827b1d6997745b952c2
Shaji <shajisuneriya@gmail.com>
2025-09-22 11:12:14 +0530

    Created testng.xml and updated pom.xml(build-plugin to take testngxml file0 to run from CLI tool

--- src/test/java/com/api/tests/MasterAPITest.java
+++ src/test/java/com/api/tests/MasterAPITest.java
@@ -21,12 +21,19 @@ public class MasterAPITest {
 				.post("/master").then().statusCode(200).time(lessThan(1000L)).body("message", equalTo("Success"))
 				.body("data", notNullValue()).body("data", hasKey("mst_oem")).body("data", hasKey("mst_model"))
 				.body("$", hasKey("message"))
-				.body("data.mst_oem" , equalTo(2))
-				.body("data.mst_model" , greaterThan(0))
+				.body("data.mst_oem.size()" , equalTo(2))
+				.body("data.mst_model.size()" , greaterThan(0))
 				.body("data.mst_model.id", everyItem(notNullValue()))
 				.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("JSONschema/MasterAPIJSONSchema.json"))
 				.log().all();
 
 	}
+	
+	@Test
+	public void masterAPIresquestNegative() {
+
+		given().baseUri(getProperty("BASE_URI")).header("Authorization","").contentType("").when()
+				.post("/master").then().statusCode(401);
+				}
 
 }
